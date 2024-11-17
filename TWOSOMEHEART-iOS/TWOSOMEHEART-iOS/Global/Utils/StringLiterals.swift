bcb25339367a0b6d3a42b348f644e617f43e67b2
cirtuare <cirtuare@gmail.com>
2024-11-17 12:20:55 +0900

    [Feat] StringLiterals 추가 (#1)

---
+++ TWOSOMEHEART-iOS/TWOSOMEHEART-iOS/Global/Utils/StringLiterals.swift
@@ -0,0 +1,41 @@
+//
+//  StringLiterals.swift
+//  TWOSOMEHEART-iOS
+//
+//  Created by 이수민 on 11/17/24.
+//
+
+import Foundation
+
+enum StringLiterals {
+    
+    enum MenuType {
+        
+        static let new = "NEW"
+        
+        static let recommend = "추천세트"
+        
+        static let coffee = "커피/음료"
+        
+        static let dessert = "디저트"
+        
+        static let food = "푸드"
+        
+        static let product = "상품"
+        
+    }
+
+    enum CoffeeType {
+        
+        static let coffee = "커피"
+        
+        static let decaffeine = "디카페인커피"
+        
+        static let drinks = "음료"
+        
+        static let tea = "티/티라떼"
+        
+        static let icecream = "아이스크림/빙수"
+        
+    }
+}
