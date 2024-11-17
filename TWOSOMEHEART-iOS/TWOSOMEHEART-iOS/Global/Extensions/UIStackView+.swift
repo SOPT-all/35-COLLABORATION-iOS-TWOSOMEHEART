0b2c41b9f573d2740ff998f81c6c1a5f4223db1e
cirtuare <cirtuare@gmail.com>
2024-11-18 08:45:54 +0900

    [Feat] UIStackView extension 추가 (#3)

---
+++ TWOSOMEHEART-iOS/TWOSOMEHEART-iOS/Global/Extensions/UIStackView+.swift
@@ -0,0 +1,16 @@
+//
+//  UIStackView+.swift
+//  TWOSOMEHEART-iOS
+//
+//  Created by 이수민 on 11/18/24.
+//
+
+import UIKit
+
+extension UIStackView {
+    
+    func addArrangedSubviews(_ views: UIView...) {
+        views.forEach { self.addArrangedSubview($0) }
+    }
+    
+}
