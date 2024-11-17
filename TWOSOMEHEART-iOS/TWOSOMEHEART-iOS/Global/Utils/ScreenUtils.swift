cd7ecc3a41288274e378a507f7a0cf1477f71ce0
cirtuare <cirtuare@gmail.com>
2024-11-17 12:15:17 +0900

    [Feat] ScreenUtils 추가 (#1)

---
+++ TWOSOMEHEART-iOS/TWOSOMEHEART-iOS/Global/Utils/ScreenUtils.swift
@@ -0,0 +1,22 @@
+//
+//  ScreenUtils.swift
+//  TWOSOMEHEART-iOS
+//
+//  Created by 이수민 on 11/17/24.
+//
+
+import UIKit
+
+struct ScreenUtils {
+    
+    // MARK: - 디바이스 width
+    static var width: CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    
+    // MARK: - 디바이스 height
+    static var height: CGFloat {
+        return UIScreen.main.bounds.height
+    }
+    
+}
