1a184575d0bbd9469073e744b29a58ae7b3e1328
RAFA <rafa71802937@gmail.com>
2024-11-20 22:09:38 +0900

    [Feat] 메뉴 옵션 헤더 UI 구현 (#4)

---
+++ TWOSOMEHEART-iOS/TWOSOMEHEART-iOS/Presentation/MenuOption/View/HeaderView/MenuOptionHeaderView.swift
@@ -0,0 +1,117 @@
+//
+//  MenuOptionHeaderView.swift
+//  TWOSOMEHEART-iOS
+//
+//  Created by RAFA on 11/20/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class MenuOptionHeaderView: UITableViewHeaderFooterView {
+
+    // MARK: - UI Properties
+
+    private let titleLabel = UILabel()
+    private let priceLabel = UILabel()
+    private let currencyUnitLabel = UILabel()
+    private let priceStackView = UIStackView()
+    private let arrowImageView = UIImageView()
+    private let addedOptionLabel = UILabel()
+
+    // MARK: - Initializers
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+
+        setStyle()
+        setHierarchy()
+        setLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - UI
+
+    private func setStyle() {
+        titleLabel.do {
+            $0.setLabel(
+                textColor: .tsBlack,
+                font: TSFont.h3r
+            )
+        }
+
+        priceLabel.do {
+            $0.setLabel(
+                numberOfLines: 1,
+                textColor: .gray80,
+                font: TSFont.b1b
+            )
+        }
+
+        currencyUnitLabel.do {
+            $0.setLabel(
+                text: "원",
+                numberOfLines: 1,
+                textColor: .gray80,
+                font: TSFont.b1b
+            )
+        }
+
+        priceStackView.do {
+            $0.addArrangedSubviews([priceLabel, currencyUnitLabel])
+            $0.setStackView(distribution: .fillProportionally)
+        }
+
+        arrowImageView.do {
+            $0.image = UIImage(resource: .optionArrowDown)
+            $0.tintColor = .tsBlack
+        }
+
+        addedOptionLabel.do {
+            $0.setLabel(
+                alignment: .left,
+                textColor: .gray70,
+                font: TSFont.c2r
+            )
+        }
+    }
+
+    private func setHierarchy() {
+        contentView.addSubviews(
+            titleLabel,
+            priceStackView,
+            arrowImageView,
+            addedOptionLabel
+        )
+    }
+
+    private func setLayout() {
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(6)
+            $0.leading.equalTo(16)
+        }
+
+        priceStackView.snp.makeConstraints {
+            $0.centerY.equalTo(titleLabel)
+            $0.trailing.equalTo(arrowImageView.snp.leading).offset(-10)
+        }
+
+        arrowImageView.snp.makeConstraints {
+            $0.centerY.equalTo(priceStackView)
+            $0.trailing.equalTo(-16)
+        }
+
+        addedOptionLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(titleLabel.snp.bottom).offset(16)
+            $0.leading.equalTo(titleLabel)
+            $0.bottom.equalTo(-12)
+            $0.height.equalTo(18)
+        }
+    }
+}
