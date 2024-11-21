9112fe1d7a02b91af6833722c3ff071022e6e2de
cirtuare <cirtuare@gmail.com>
2024-11-21 17:37:40 +0900

    [Feat] ModalViewController 기본 설정 (#8)

--- TWOSOMEHEART-iOS/TWOSOMEHEART-iOS/Presentation/ModalView/Controller/ModalViewController.swift
+++ TWOSOMEHEART-iOS/TWOSOMEHEART-iOS/Presentation/ModalView/Controller/ModalViewController.swift
@@ -5,3 +5,31 @@
 //  Created by 이수민 on 11/19/24.
 //
 
+import UIKit
+
+import SnapKit
+import Then
+
+class ModalViewController: BaseViewController {
+    
+    let modalView = ModalView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setHierarchy()
+        setLayout()
+        setStyle()
+    }
+    
+    override func setHierarchy() {
+        view.addSubview(modalView)
+    }
+    
+    override func setLayout() {
+        modalView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+}
