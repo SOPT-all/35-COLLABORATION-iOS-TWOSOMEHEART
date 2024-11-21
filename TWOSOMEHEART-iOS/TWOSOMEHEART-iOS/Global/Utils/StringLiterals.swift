12215db0a76c56f0cb0c260847a2ef4df4d11d9b
RAFA <rafa71802937@gmail.com>
2024-11-21 19:00:07 +0900

    [Add] 메뉴 옵션 StringLiterals 추가 (#4)

--- TWOSOMEHEART-iOS/TWOSOMEHEART-iOS/Global/Utils/StringLiterals.swift
+++ TWOSOMEHEART-iOS/TWOSOMEHEART-iOS/Global/Utils/StringLiterals.swift
@@ -38,4 +38,27 @@ enum StringLiterals {
         static let icecream = "아이스크림/빙수"
         
     }
+
+    enum MenuOptionType {
+
+        static let density = "농도"
+
+        static let addShot = "샷 추가"
+
+        static let vanillaSyrup = "바닐라시럽"
+
+        static let hazelnutSyrup = "헤이즐넛시럽"
+
+        static let caramelSyrup = "카라멜시럽"
+
+        static let whippedCream = "휘핑크림"
+
+        static let caramelDrizzle = "카라멜드리즐"
+
+        static let chocolateDrizzle = "초콜릿드리즐"
+
+        static let pricePerPlusOne = "(+1에 500원)"
+
+    }
+
 }
