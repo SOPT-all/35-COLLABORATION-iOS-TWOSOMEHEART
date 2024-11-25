80a0ef9c232b624d51478236f0abb1bb3774451e
cirtuare <cirtuare@gmail.com>
2024-11-25 15:10:24 +0900

    [Feat] ModalInfo 추가 (#7)

---
+++ TWOSOMEHEART-iOS/TWOSOMEHEART-iOS/Presentation/ModalView/Model/ModalInfo.swift
@@ -0,0 +1,19 @@
+//
+//  ModalInfo.swift
+//  TWOSOMEHEART-iOS
+//
+//  Created by 이수민 on 11/25/24.
+//
+
+import Foundation
+
+struct ModalInfo {
+    
+    var price: Int
+    var personalOption: String
+    
+}
+
+extension ModalInfo {
+    static let modalInfo = ModalInfo(price: 5900, personalOption: "")
+}
