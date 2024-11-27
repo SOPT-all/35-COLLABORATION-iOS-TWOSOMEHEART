ff08ca0b0276cd471ca7a9beefbd6a73052f8627
RAFA <rafa71802937@gmail.com>
2024-11-27 21:08:07 +0900

    [Feat] 메뉴 서브 카테고리 타입 구현 (#19)

---
+++ TWOSOMEHEART-iOS/TWOSOMEHEART-iOS/Presentation/Menu/View/Type/MenuSubcategoryType.swift
@@ -0,0 +1,16 @@
+//
+//  MenuSubcategoryType.swift
+//  TWOSOMEHEART-iOS
+//
+//  Created by RAFA on 11/27/24.
+//
+
+import Foundation
+
+enum MenuSubcategoryType: String, CaseIterable {
+    case coffee = "커피"
+    case decaffeinatedCoffee = "디카페인 커피"
+    case beverages = "음료"
+    case teaAndTeaLatte = "티/티라떼"
+    case iceCreamAndShavedIce = "아이스크림/빙수"
+}
