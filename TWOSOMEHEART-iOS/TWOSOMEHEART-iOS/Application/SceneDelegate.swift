a990b4e836699436b36facb4aad901fdd8643c05
RAFA <rafa71802937@gmail.com>
2024-11-28 05:56:30 +0900

    [Feat] 루트 뷰 수정 (#19)

--- TWOSOMEHEART-iOS/TWOSOMEHEART-iOS/Application/SceneDelegate.swift
+++ TWOSOMEHEART-iOS/TWOSOMEHEART-iOS/Application/SceneDelegate.swift
@@ -14,9 +14,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
-            
+
+        let menuVC = MenuViewController()
+
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
+        window?.rootViewController = UINavigationController(rootViewController: menuVC)
+        window?.backgroundColor = .tsWhite
         window?.makeKeyAndVisible()
     }
 
