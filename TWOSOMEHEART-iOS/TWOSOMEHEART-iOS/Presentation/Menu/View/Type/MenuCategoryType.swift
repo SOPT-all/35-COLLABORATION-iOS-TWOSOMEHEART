18558f651ef31ce39723dac41f0a3f995e245d98
RAFA <rafa71802937@gmail.com>
2024-11-27 21:07:46 +0900

    [Feat] 메뉴 메인 카테고리 타입 구현 (#19)

---
+++ TWOSOMEHEART-iOS/TWOSOMEHEART-iOS/Presentation/Menu/View/Type/MenuCategoryType.swift
@@ -0,0 +1,17 @@
+//
+//  MenuCategoryType.swift
+//  TWOSOMEHEART-iOS
+//
+//  Created by RAFA on 11/27/24.
+//
+
+import Foundation
+
+enum MenuCategoryType: String, CaseIterable {
+    case new = "NEW"
+    case recommendedSet = "추천세트"
+    case coffeeAndBeverages = "커피/음료"
+    case dessert = "디저트"
+    case food = "음식"
+    case product = "상품"
+}
