a415fd1335b69148752b487a112758de33edc47c
RAFA <rafa71802937@gmail.com>
2024-11-28 03:31:35 +0900

    [Chore] 오타 수정 (#19)

--- TWOSOMEHEART-iOS/TWOSOMEHEART-iOS/Presentation/Menu/View/Type/MenuCategoryType.swift
+++ TWOSOMEHEART-iOS/TWOSOMEHEART-iOS/Presentation/Menu/View/Type/MenuCategoryType.swift
@@ -12,6 +12,6 @@ enum MenuCategoryType: String, CaseIterable {
     case recommendedSet = "추천세트"
     case coffeeAndBeverages = "커피/음료"
     case dessert = "디저트"
-    case food = "음식"
+    case food = "푸드"
     case product = "상품"
 }
